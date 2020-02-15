bf07ae3bf064186ab4364a7c8927f9620337a965
dparradaza <daparrad@gmail.com>
2020-02-15 15:11:22 -0500

    primer commit

---
+++ main.py
@@ -0,0 +1,14 @@
+# -*- coding: utf-8 -*-
+"""
+Editor de Spyder
+
+Este es un archivo temporal.
+"""
+from kivy.app import App
+from kivy.uix.button import Button
+
+class TestApp(App):
+    def build(self):
+        return Button(text='Hello World')
+
+TestApp().run()
